b235041a44e9508772a3e50023a63763796dd1c9
VisinSky <visin@live.cn>
2026-02-01 15:37:56 +0800

    Update diy-part2.sh

--- FanchmWrt/diy-part2.sh
+++ FanchmWrt/diy-part2.sh
@@ -9,6 +9,24 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+#!/bin/bash
+
+echo "优化 ddns-go 编译配置"
+# 调整 Go 编译参数
+cat >> .config << EOF
+CONFIG_PACKAGE_ddns-go=y
+EOF
+
+# 优化 Go 编译选项
+if grep -q "Go" feeds/ddnsgo/ddns-go/Makefile; then
+  echo "找到 ddns-go 的 Makefile，进行优化"
+  # 减少并行编译以节省内存
+  sed -i '/^define Build\/Compile/i \
+define Build/Compile \
+\t$(call GoPackage/Compile) \
+\tGOMAXPROCS=2 GOGC=50 $(GO_PKG_BUILD_VARS) \\
+' feeds/ddnsgo/ddns-go/Makefile
+fi
 
 # 修改默认LAN口IP地址 Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
